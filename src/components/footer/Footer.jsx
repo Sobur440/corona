100caeb3e278905e42f462d30f70891acd2c8d76
Sobur Abubakar <104511346+Sobur440@users.noreply.github.com>
2023-09-26 17:54:02 +0100

    Update Footer.jsx

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -56,7 +56,7 @@ const Footer = () => {
            </div>
            <div className="developer">
             <div className="by">Developed by Sobur Abubakar <span className='halal'>(D Halal Uchiha)</span></div>
-            <div className="pc-container"><a href="https://twitter.com/Wave440" target="_blank" className='pc'>buy me a pc</a></div>
+{/*             <div className="pc-container"><a href="https://twitter.com/Wave440" target="_blank" className='pc'>buy me a pc</a></div> */}
            </div>
 
         </div>
